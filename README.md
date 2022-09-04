6ee0fe45623796a12eb5d90c05888b15569a646e
Mark Elliot <mark.elliot@gmail.com>
2022-09-04 10:59:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# autorelease
+
+Automatically creates a release based on time and the presence of commits since the latest release.
+
+## Configuring this action
+
+To configure this action create a file in your `.github/workflows` directory:
+
+```yaml
+name: autorelease
+on:
+  schedule:
+    # check at 11am every day
+    - cron: "0 11 * * *"
+jobs:
+  autorelease:
+    name: autorelease
+    runs-on: ubuntu-latest
+    steps:
+      - name: autorelease
+        uses: markelliot/autorelease@v1
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          # maximum number of days since last release
+          max-days: 7
+```
+
+Note
+
+- This action works when the versioning scheme matches the pattern `^\d+\.\d+\.\d+$`.
+- A new release will only be created if there exists at least one prior release and there have been commits since the last release.
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+// milliseconds/second * seconds/minute * minutes/hour * hours/day = mlliseconds/day
+const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// compatible with releases of the form <major>.<minor>.<patch>
+const RELEASE_STYLE = /^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)$/;
+
+async function run(): Promise<void> {
+  try {
+    const authToken = core.getInput("github-token");
+    const maxDays = parseInt(core.getInput("max-days"));
+    const octokit = github.getOctokit(authToken);
+    const context = github.context;
+
+    const latestRelease = await octokit.rest.repos.getLatestRelease({
+      ...context.repo,
+    });
+    if (latestRelease.data === undefined) {
+      core.warning(
+        "Unable to perform an autorelease if there are no prior releases."
+      );
+      return;
+    }
+    const releaseAge =
+      (Date.now() - Date.parse(latestRelease.data.created_at)) /
+      MILLISECONDS_PER_DAY;
+    core.info(
+      `Found latest release ${latestRelease.data.tag_name} from ${releaseAge} days ago`
+    );
+
+    const releaseComponents = latestRelease.data.tag_name.match(RELEASE_STYLE);
+    if (releaseComponents === null) {
+      core.warning(
+        `Unable to perform an autorelease because last release does not match required tag pattern <major>.<minor>.<patch> (${latestRelease.data.tag_name})`
+      );
+      return;
+    }
+
+    // check that there have been changes since last release
+    const commitsSinceLastRelease = await octokit.rest.repos.listCommits({
+      ...context.repo,
+      since: latestRelease.data.tag_name,
+    });
+    if (
+      commitsSinceLastRelease.data === undefined ||
+      commitsSinceLastRelease.data.length < 1
+    ) {
+      core.warning("No commits since last release");
+      return;
+    }
+
+    if (releaseAge >= maxDays) {
+      const newTag = `${releaseComponents[1]}.${
+        parseInt(releaseComponents[2]) + 1
+      }.0}`;
+      core.info(`Creating a new release ${newTag}`);
+      await octokit.rest.repos.createRelease({
+        ...context.repo,
+        tag_name: newTag,
+        name: newTag,
+        generate_release_notes: true,
+      });
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  }
+}
+
+run();
